7f8730b79f1fc74eed608833e96641aed9987f55
Pietro Monteiro <pietro@riseup.net>
2017-03-28 21:08:30 -0700

    Pass -march and -mthumb.

--- build.rs
+++ build.rs
@@ -664,7 +664,8 @@ fn cc(file: &Path, ext: &str, target: &Target, out_dir: &Path) -> Command {
         // Needed for Android NDK Unified Headers, see:
         // https://android.googlesource.com/platform/ndk/+/master/docs/UnifiedHeaders.md#Supporting-Unified-Headers-in-Your-Build-System
         //let _ = c.define("__ANDROID_API__", Some("18"));
-        //let _ = c.flag("-march=armv7-a");
+        let _ = c.flag("-march=armv7-a");
+        let _ = c.flag("-mthumb");
         let _ = c.define("BORINGSSL_CLANG_SUPPORTS_DOT_ARCH", Some("1"));
     }
 
